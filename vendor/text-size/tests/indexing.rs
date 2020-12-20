29967ef60bac8688f269f9121bbf7498d47d714e
Ximin Luo <infinity0@debian.org>
2020-12-20 15:06:41 +0000

    New upstream version 1.49.0~beta.4+dfsg1

--- vendor/text-size/tests/indexing.rs
+++
@@ -1,8 +0,0 @@
-use text_size::*;
-
-#[test]
-fn main() {
-    let range = TextRange::default();
-    &""[range];
-    &String::new()[range];
-}
--- vendor/ungrammar/README.md
+++
@@ -1,5 +0,0 @@
-# ungrammar
-
-A DLS for specifying concrete syntax tree.
-
-See [./rust.ungram](./rust.ungram) for an example.
