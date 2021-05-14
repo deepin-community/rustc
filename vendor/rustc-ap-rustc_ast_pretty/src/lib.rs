f20569fa03b3b370f70f0df777c134d7f38d09e9
Ximin Luo <infinity0@debian.org>
2021-05-14 18:42:13 +0100

    New upstream version 1.52.1+dfsg1

---
+++ vendor/rustc-ap-rustc_ast_pretty/src/lib.rs
@@ -0,0 +1,9 @@
+#![feature(bool_to_option)]
+#![feature(crate_visibility_modifier)]
+#![feature(or_patterns)]
+#![feature(box_patterns)]
+#![recursion_limit = "256"]
+
+mod helpers;
+pub mod pp;
+pub mod pprust;
